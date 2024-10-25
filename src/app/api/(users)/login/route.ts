77861a8a5c0eebbff9c51e2573df0ab270630929
Venkatesh3696 <venkateshpentakota888@gmail.com>
2024-10-25 20:31:06 +0530

    fix type errors

--- src/app/api/(users)/login/route.ts
+++ src/app/api/(users)/login/route.ts
@@ -1,15 +1,17 @@
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import User from '@/models/users';
-import syncDatabase from '@/lib/db/syncDatabase';
+
 import { generateToken } from '@/lib/helpers/jwt';
 
 export async function POST(request: Request) {
 	try {
 		const { email, password } = await request.json();
-		await syncDatabase();
 
-		const user = await User.findOne({ where: { email } });
+		const user: {
+			id: number;
+			password: string;
+		} | null = await User.findOne({ where: { email } });
 
 		if (!user) {
 			return new Response('User not found', { status: 404 });
